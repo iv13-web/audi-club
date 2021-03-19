9069a8ada8175482b6b2efe79605849b913ea1b1
iv13-web <iivanovspg@gmail.com>
2021-03-19 18:05:31 +0300

    video buttons upgrd

--- js/main.js
+++ js/main.js
@@ -1,79 +1,77 @@
-// let swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 2,
-//     spaceBetween: 30,
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     breakpoints: {
-//         500: {
-//             slidesPerView: 1
-//         },
-//         1000: {
-//             slidesPerView: 2
-//         },
-//         1200: {
-//             slidesPerView: 3
-//         }
-//     }
-//   });
-
-
-  let swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      }, 
+      1000: {
+          slidesPerView: 2
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1
-        }, 
-        1000: {
-            slidesPerView: 2
-        },
-        1200: {
-            slidesPerView: 3
-        }
+      1200: {
+          slidesPerView: 3
       }
-  });
+    }
+});
 
 
-let playButton = document.querySelector('.play')
-let pauseButton = document.querySelector('.pause')
-let video = document.getElementById('video')
 
-// Запуск видео по клику на кнопку play
-playButton.addEventListener('click', function() {
-  video.play();
-  this.classList.add('remove')
-  pauseButton.classList.remove('remove')
+
+let video = document.getElementById("video");
+let button = document.querySelector('.button')
+let playSVG = document.querySelector('.play-svg')
+let pauseSVG = document.querySelector('.pause-svg')
+let isPlaying = false;
+
+function togglePlay() {
+  if (isPlaying) {
+    video.pause()
+  } else {
+    video.play()
+  }
+};
+
+video.onplaying = function() {
+  isPlaying = true;
+};
+video.onpause = function() {
+  isPlaying = false;
+};
+
+// нажатия на кнопку
+button.addEventListener('click', function() {
+  togglePlay();
+  if (isPlaying == false) {
+    playSVG.classList.add('remove');
+    pauseSVG.classList.remove('remove');
+    button.classList.add('transparent');
+  }
+  else {
+    playSVG.classList.remove('remove');
+    pauseSVG.classList.add('remove');
+    button.classList.remove('transparent');
+  }
 })
 
-// Пауза видео по клику на кнопку pause
-pauseButton.addEventListener('click', function () {
-  video.pause();
-  playButton.classList.remove('remove')
-  this.classList.add('remove') 
-})
-
-// Скрытие кнопки pause при перемещении курсора с неё
-pauseButton.addEventListener('mouseleave', function() {
-  this.classList.add('transparent')
-})
-
-// Показ кнопки pause при перемещении курсора с неё
-pauseButton.addEventListener('mouseenter', function() {
-  this.classList.remove('transparent')
-}, false)
-
-// Показ кнопки play при нажатии в любое место на видео
-video.addEventListener('click', function () {
-  playButton.classList.remove('remove')
-  this.pause();
+// нажатия на любое местов в окне видео
+video.addEventListener('click', function() {
+  togglePlay();
+  if (isPlaying == false) {
+    playSVG.classList.add('remove');
+    pauseSVG.classList.remove('remove')
+    button.classList.add('transparent');
+  }
+  else {
+    playSVG.classList.remove('remove');
+    pauseSVG.classList.add('remove');
+    button.classList.remove('transparent');
+  }
 })
